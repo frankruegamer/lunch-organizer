dc42bb6d3ca8bd489f65c9237d5076013ef88dbe
Frank Rügamer <frankruegamer@gmail.com>
2019-06-14 17:45:20 +0200

    Add doenmez doener doener data

---
+++ src/main/resources/data/doenmez_doener/doener.sql
@@ -0,0 +1,14 @@
+'1', 'Drehspieß', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten und Soße', 4.90
+'2', 'Drehspieß Pur', 'mit Drehspießfleisch und Soße', 4.60
+'3', 'Studenten / Schüler Drehspieß', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten und Soße', 4.60
+'4', 'Drehspieß Spezial', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten, Käse und Soße', 5.50
+'5', 'Dönmez Drehspieß', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten, Oliven, Käse und Soße', 5.50
+'6', 'Royal Drehspieß', 'mit Drehspießfleisch, Gemischter Salat, Bratkartoffeln und Pizzakäse', 6.00
+'7', 'Döner-Box', 'mit Drehspießfleisch, Kartoffeln, Pommes oder Salat', 5.00
+'8', 'Monster Drehspieß', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten und Soße', 6.50
+'9', 'Cheese-Drehspieß', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten, Käse überbacken und Soße', 5.50
+'10', 'Dürüm Drehspieß', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten und Soße', 5.50
+'11', 'Dürüm Drehspieß Spezial', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten,Käse und Soße', 6.00
+'12', 'Kinder Drehspieß', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten, Soße', 4.00
+'13', 'Drehspieß Teller', 'mit Drehspießfleisch, Kartoffeln, Pommes oder Reis grüner Salat, Krautsalat, Zwiebeln, Tomaten, Peperoni, Käse und Soße', 8.50
+'14', 'kleiner Drehspieß Teller', 'mit Krautsalat, Zwiebeln, Tomaten, Peperoni, Käse und Soß mit Drehspießfleisch, Kartoffeln, Pommes oder Reis', 7.00

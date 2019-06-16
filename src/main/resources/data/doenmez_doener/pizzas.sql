c0fdb35b0c8048da510918c713c47e71f95c7248
Frank <frankruegamer@gmail.com>
2019-06-16 16:27:51 +0200

    Add doenmez doener pizza data

---
+++ src/main/resources/data/doenmez_doener/pizzas.sql
@@ -0,0 +1,18 @@
+'60', 'Pizza Margherita', null, 5.00
+'61', 'Pizza Rindersalami', null, 5.50
+'62', 'Pizza mit Rinderpeperoniwurst', null, 5.50
+'63', 'Pizza Putenschinken', null, 5.50
+'64', 'Pizza mit Champignons', null, 5.00
+'65', 'Pizza mit Knoblauch', 'und Zwiebeln', 5.50
+'66', 'Pizza mit Drehspießfleisch', null, 7.50
+'67', 'Pizza mit Rindersalami', 'und Putenschinken', 6.00
+'68', 'Pizza Mary', 'und Rindersalami, Putenschinken, Champignons', 6.00
+'69', 'Pizza Italia', 'mit Paprika Putenschinken und Ei', 6.50
+'70', 'Pizza Calzone', 'gefüllt mit Rindersalami, Putenschinken und Champignons', 7.00
+'71', 'Pizza Mozzarella', 'mit Tomatenscheiben und Basilikum', 7.00
+'72', 'Pizza Hawaii', 'mit Putenschinken und Ananas', 6.50
+'73', 'Pizza Vier Jahreszeiten', 'mit Putenschinken, Rindersalami, Paprika und Champignons', 7.00
+'74', 'Pizza Tonno', 'mit Thunfisch, Zwiebeln und Knoblauch', 7.50
+'75', 'Pizza Drehspieß-Calzone', 'gefüllt mit Drehspießfleisch, Salat, Zwiebeln, Weißkraut und Knoblauchsoße', 7.50
+'76', 'Pizza Spezial', 'mit Rindersalami, Putenschinken, Champignons und grüner Peperoni', 7.50
+'77', 'Pizza Vegetarisch', 'mit Broccoli, frischen Champignons, Paprika, Zwiebeln, Mais und Oliven', 7.50

e5b17240af934d7e8a20e1ca6fee4fc1d54b2202
Frank <frankruegamer@gmail.com>
2019-06-16 15:48:53 +0200

    Add doenmez doener lahmacun data

---
+++ src/main/resources/data/doenmez_doener/lahmacun.sql
@@ -0,0 +1,4 @@
+'30', 'Lahmacun', 'mit grüner Salat, Krautsalat, Zwiebeln, Tomaten und Soße', 5.00
+'31', 'Lahmacun Pur', 'mit Drehspießfleisch und Soße', 5.00
+'32', 'Lahmacun Gerolt', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten und Soße', 5.50
+'33', 'Lahmacun Spezial', 'mit Drehspießfleisch, grüner Salat, Krautsalat, Zwiebeln, Tomaten, Käse und Soße', 6.00

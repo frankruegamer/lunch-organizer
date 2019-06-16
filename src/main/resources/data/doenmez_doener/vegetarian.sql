b4bf493c8bef5b33210d01c8303b4d4735d63453
Frank <frankruegamer@gmail.com>
2019-06-16 16:03:36 +0200

    Add doenmez doener "vegetarian" data

---
+++ src/main/resources/data/doenmez_doener/vegetarian.sql
@@ -0,0 +1,7 @@
+'45', 'Vegetarischer im Brot', 'mit grüner Salat, Krautsalat, Zwiebeln, Tomaten, Auberginen, Käser und Soße', 5.00
+'46', 'Vegetarischer Dürüm', 'mit grüner Salat, Krautsalat, Zwiebeln, Tomaten, Auberginen, Käser und Soße',  5.00
+'47', 'Vegetarischer Teller', 'mit Kartoffeln oder Pommes frites, grüner Salat, Krautsalat. Zwiebeln, Tomaten, Peperoni, Käser und Soße', 7.50
+'48', 'Falafel', 'mit pürierte und gewürzte Kichererbsen, Käse, grüner Salat, Krautsalat, Zwiebeln, Tomaten und Soße', 5.00
+'49', 'Falafel Teller', 'mit Kartoffeln oder Pommes, grüner Salat, Krautsalat, Zwiebeln, Tomaten, Peperoni, Auberginen, Käse und Soße', 8.00
+'50', 'Veganer im Fladenbrot', 'mit grüner Salat, Krautsalat. Zwiebeln, Tomaten, Auberginen und Knoblauch Öl', 5.50
+'51', 'Kalamariteller', 'mit Kartoffeln oder Pommes frites und Salat', 8.50

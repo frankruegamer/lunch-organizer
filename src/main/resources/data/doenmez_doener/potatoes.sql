45f70588b6d044b536648b662672b32f57114335
Frank <frankruegamer@gmail.com>
2019-06-16 16:07:48 +0200

    Add doenmez doener potatoe data

---
+++ src/main/resources/data/doenmez_doener/potatoes.sql
@@ -0,0 +1,4 @@
+'55', 'Knoblauchkartoffeln', 'null', 4.00
+'56', 'Knoblauchkartoffeln Spezial klein', 'mit Drehspießfleisch und Soße', 7.00
+'57', 'Knoblauchkartoffeln Spezial groß', 'mit Drehspießfleisch und Soße', 8.50
+'58', 'Portion Pommes Frites', 'mit Ketchup oder Nayonnaise', 3.90

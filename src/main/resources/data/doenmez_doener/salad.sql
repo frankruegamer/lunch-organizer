75fbbf3c394e7a8445c36f3a3e00702388f71322
Frank <frankruegamer@gmail.com>
2019-06-16 15:54:28 +0200

    Add doenmez doener salad data

---
+++ src/main/resources/data/doenmez_doener/salad.sql
@@ -0,0 +1,8 @@
+'35', 'Hirtensalat', null, 6.00
+'36', 'Krautsalat', null, 5.00
+'37', 'Nudelsalat', null, 6.00
+'38', 'Gemischter Salat', null, 6.00
+'39', 'Dönmez Salat', null, 6.00
+'40', 'Auberginen Salat', null, 6.00
+'41', 'Beilagen Salat', null, 5.00
+'42', 'Italienisch Salat', 'Grüner Salat, Kraut, Zwiebeln, Hirtensalat, Putenschinken, Mozzarella. Gekochter Eier, Oliven Öl', 7.00

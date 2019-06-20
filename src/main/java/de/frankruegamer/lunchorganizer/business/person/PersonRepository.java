7afd75da17a83b9041b9b8dead7a61e98d4ebea9
Frank <frankruegamer@gmail.com>
2019-06-20 17:53:26 +0200

    Optimize imports

--- src/main/java/de/frankruegamer/lunchorganizer/business/person/PersonRepository.java
+++ src/main/java/de/frankruegamer/lunchorganizer/business/person/PersonRepository.java
@@ -1,8 +1,6 @@
 package de.frankruegamer.lunchorganizer.business.person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 

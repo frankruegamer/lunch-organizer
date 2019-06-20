dc54b2967f036c24f89bb97a16d0419fe8d26b03
Frank <frankruegamer@gmail.com>
2019-06-20 18:38:03 +0200

    Add repository method to get latest order by timestamp, path /search/latest

--- src/main/java/de/frankruegamer/lunchorganizer/business/order/RestaurantOrderRepository.java
+++ src/main/java/de/frankruegamer/lunchorganizer/business/order/RestaurantOrderRepository.java
@@ -2,8 +2,12 @@ package de.frankruegamer.lunchorganizer.business.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(collectionResourceRel = "orders", path = "orders")
 public interface RestaurantOrderRepository extends JpaRepository<RestaurantOrder, Long> {
+
+	@RestResource(rel = "latest", path = "latest")
+	RestaurantOrder findFirstByOrderByTimestampDesc();
 
 }

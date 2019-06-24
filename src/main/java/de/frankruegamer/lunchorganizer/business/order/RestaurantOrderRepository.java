1ce97f47ca89b4b102b0cf74b4daba37ede95100
Frank Rügamer <frankruegamer@gmail.com>
2019-06-24 18:02:06 +0200

    Add top 10 query method

--- src/main/java/de/frankruegamer/lunchorganizer/business/order/RestaurantOrderRepository.java
+++ src/main/java/de/frankruegamer/lunchorganizer/business/order/RestaurantOrderRepository.java
@@ -4,10 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "orders", path = "orders")
 public interface RestaurantOrderRepository extends JpaRepository<RestaurantOrder, Long> {
 
 	@RestResource(rel = "latest", path = "latest")
 	RestaurantOrder findFirstByOrderByTimestampDesc();
+
+	@RestResource(rel = "last10", path = "last10")
+	List<RestaurantOrder> findTop10ByOrderByTimestampDesc();
 
 }

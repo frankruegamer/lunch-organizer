4a8d4a06227804b81ccfc7f3dfd3fce8ac5b5b5c
Frank <frankruegamer@gmail.com>
2019-06-29 17:17:03 +0200

    Add paged method to get food by restaurant and regex

--- src/main/java/de/frankruegamer/lunchorganizer/business/food/FoodRepository.java
+++ src/main/java/de/frankruegamer/lunchorganizer/business/food/FoodRepository.java
@@ -1,7 +1,18 @@
 package de.frankruegamer.lunchorganizer.business.food;
 
+import de.frankruegamer.lunchorganizer.business.restaurant.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
+
+	@Query(value = "SELECT * " +
+	               "FROM food " +
+	               "WHERE (food.name ~* :regex OR description ~* :regex)" +
+	               "  AND restaurant_id = :restaurant", nativeQuery = true)
+	Page<Food> findByNameRegex(@Param("regex") String regex, @Param("restaurant") Restaurant restaurant, Pageable page);
 
 }
